14bd14c7304e1560ecab0a29dcbdba7886ba9929
“LeeYuChul” <yoochul89@naver.com>
2024-12-19 23:58:47 +0900

    [CHORE] 초대장 첫 커미

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import InvitationImage from './assets/Invitation.jpeg';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={InvitationImage} alt="Invitation" className="centered-image" />
+    </div>
+  );
+}
+
+export default App;
